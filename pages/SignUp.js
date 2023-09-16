8f9c60b25038f0cc51af0b4cc518c411f241c205
KevMP <109618100+KevMP@users.noreply.github.com>
2023-09-16 17:26:09 -0400

    VIdeo shit

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Picker, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Picker, StyleSheet, Dimensions } from 'react-native';
+import Video from 'react-native-video';
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
     paddingHorizontal: 16,
   },
   header: {
@@ -43,82 +45,72 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginVertical: 12,
   },
+  videoBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width,
+    height,
+  },
 });
 
 function Signup() {
-  const [step, setStep] = useState(1);
-  const [username, setUsername] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [skillCategory, setSkillCategory] = useState('STEM');
-  const [specificSkill, setSpecificSkill] = useState('');
-  const [description, setDescription] = useState('');
-  const [profilePicture, setProfilePicture] = useState(null);
-
-  const nextStep = () => {
-    if (step === 1 && username && name && email && password) {
-      setStep(2);
-    } else if (step === 2 && skillCategory && specificSkill) {
-      setStep(3);
-    }
-  };
-
-  const prevStep = () => {
-    if (step > 1) {
-      setStep(step - 1);
-    }
-  };
-
+  const [selectedValue, setSelectedValue] = useState('STEM');
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>
-        {step === 1 ? 'Basic Info' : step === 2 ? 'Interests and Strengths' : 'About You'}
-      </Text>
-      <View style={styles.section}>
-        {step === 1 && (
-          <>
-            <TextInput placeholder="Username" value={username} onChangeText={setUsername} style={styles.input} />
-            <TextInput placeholder="Name" value={name} onChangeText={setName} style={styles.input} />
-            <TextInput placeholder="Email" value={email} onChangeText={setEmail} style={styles.input} />
-            <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry style={styles.input} />
-          </>
-        )}
-        {step === 2 && (
-          <>
-            <Picker selectedValue={skillCategory} onValueChange={(itemValue) => setSkillCategory(itemValue)} style={styles.picker}>
-              <Picker.Item label="STEM" value="STEM" />
-              <Picker.Item label="Communications" value="Communications" />
-            </Picker>
-            <TextInput placeholder="Specific Skill" value={specificSkill} onChangeText={setSpecificSkill} style={styles.input} />
-          </>
-        )}
-        {step === 3 && (
-          <>
-            <TextInput
-              placeholder="Describe Yourself"
-              value={description}
-              onChangeText={setDescription}
-              multiline
-              numberOfLines={4}
-              style={styles.input}
-            />
-            <TouchableOpacity>
-              <Text>Upload Profile Picture (Optional)</Text>
-            </TouchableOpacity>
-          </>
-        )}
-      </View>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        {step > 1 && (
-          <TouchableOpacity onPress={prevStep} style={[styles.button, { marginRight: 8 }]}>
-            <Text style={styles.buttonText}>Previous</Text>
-          </TouchableOpacity>
-        )}
-        <TouchableOpacity onPress={nextStep} style={styles.button}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-      </View>
+      <Video
+        source={require('../assets/background.mp4')}
+        rate={1.0}
+        volume={1.0}
+        muted={false}
+        resizeMode="cover"
+        repeat
+        style={styles.videoBackground}
+      />
+
+      <Text style={styles.header}>Sign Up</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Full Name"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+      />
+
+      <Picker
+        selectedValue={selectedValue}
+        style={styles.picker}
+        onValueChange={(itemValue) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="STEM" value="STEM" />
+        <Picker.Item label="Communication" value="Communication" />
+      </Picker>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Specific Skills"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="About Me"
+        multiline
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 }
